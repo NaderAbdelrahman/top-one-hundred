82aa3188931dd55db1966ab58bf05018ceef1ef9
Nader Abdelrahman <nader.abdelrahman@medicasafe.com>
2020-02-19 18:04:04 -0500

    Boiler Plate directory structure and Code Cleaned Up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+import './scss/app.scss'
+
+function App() {
+  return (
+      <BrowserRouter>
+        <Route></Route>
+      </BrowserRouter>
+  );
+}
+
+export default App;
